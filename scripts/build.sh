ed52621ecf1dda724c4cca5426393869de25adb2
liligeng <lilg1@ifeng.com>
2017-08-01 17:51:58 +0800

    modify ip

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,18 +3,18 @@
 # apollo config db info
 apollo_config_db_url=jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8
 apollo_config_db_username=root
-apollo_config_db_password=
+apollo_config_db_password=P@ssword2
 
 # apollo portal db info
 apollo_portal_db_url=jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8
 apollo_portal_db_username=root
-apollo_portal_db_password=
+apollo_portal_db_password=P@ssword2
 
 # meta server url
-dev_meta=http://localhost:8080
-fat_meta=http://localhost:8080
-uat_meta=http://localhost:8080
-pro_meta=http://localhost:8080
+dev_meta=http://10.21.6.95:8080
+#fat_meta=http://localhost:8080
+uat_meta=http://10.21.6.96:8080
+pro_meta=http://10.21.6.97:8080
 
 META_SERVERS_OPTS="-Ddev_meta=$dev_meta -Dfat_meta=$fat_meta -Duat_meta=$uat_meta -Dpro_meta=$pro_meta"
 
